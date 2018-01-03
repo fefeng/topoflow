1c7514e98816e03da71beac0a1c7e9338fa6f6b7
feng <fefeng0504@163.com>
2018-01-03 16:21:25 +0800

    dev

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    disable: process.env.NODE_ENV === 'development',
+    filename: 'static/css/[name].[hash].[id].css'
+});
+
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir);
+}
+
+module.exports = {
+
+    entry: {
+        main: './src/main.js', // 程序入口        
+    },
+
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: resolve('dist'),
+        publicPath: '/',
+        filename: 'js/[name].[hash].[id].js',
+        chunkFilename: 'js/[name].[hash].[id].js'
+    },
+
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src')
+        }
+    },
+
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }],
+                fallback: 'style-loader'
+            }),
+            exclude: /node_modules/
+        }, {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+            loader: 'file-loader',
+            options: {
+                name: 'static/fonts/[name]-[hash].[ext]'
+            }
+        }]
+    },
+
+    plugins: [
+        extractSass,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'static/js/[name].[hash].[id].js'
+        }),
+        new webpack.NoEmitOnErrorsPlugin(), //  允许中断不终止程序
+        new webpack.HotModuleReplacementPlugin(), //  代码热替换
+        new HtmlWebpackPlugin({ //  将webpack生成的文件在html中引用
+            filename: 'index.html', //  文件路径
+            template: './index.html', //  文件模板
+            minify: {
+                removeComments: true, //  移除HTML中的注释
+                collapseWhitespace: true //  删除空白符与换行符
+            }
+        })
+    ],
+};
